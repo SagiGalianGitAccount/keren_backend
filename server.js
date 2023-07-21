a61547a3739bfd891908c8528fe15a429bd20f5e
SagiGalianGitAccount <117572202+SagiGalianGitAccount@users.noreply.github.com>
2023-07-21 19:14:20 +0300

    just do this

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const corsOptions = {
     origin: [
-      "https://prismatic-queijadas-0e8da0.netlify.app",
+      "https://singular-gumption-1efb7d.netlify.app",
       "http://localhost:3000",
     ],
     allowedHeaders: ["Content-Type"],
